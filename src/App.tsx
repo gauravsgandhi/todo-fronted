c37dbf5c6c7a25bb41835399fced0ab60f486cc5
sgauravgandhi <gaurav.s.gandhi@philips.com>
2026-02-04 18:12:13 +0530

    Few configuration files changes are done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import TodoApp from "./components/TodoApp";
 
 const App = () => {
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, ReactElement } from "react";
+import { useEffect, useState } from "react";
+import type { ReactElement } from "react";
 import axios from "axios";
 
 type Task = {
