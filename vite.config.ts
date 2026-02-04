92a426a5f357120b91cae0ff3ba818ecbad62c6c
sgauravgandhi <gaurav.s.gandhi@philips.com>
2026-02-04 18:33:22 +0530

    Few configuration files changes are done

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',                  // important if you previously set a subpath
-  build: { outDir: 'dist' }
 });
-
